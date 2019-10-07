8dcc8a81bad040090d117589d244e3a06436a718
Sandra Valero Ponte <svalerop@vectoritcgroup.com>
2019-10-07 18:01:18 +0200

    Problemas href

--- src/main/java/com/project/controllers/MainController.java
+++ src/main/java/com/project/controllers/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 
+
+
 @Controller
 public class MainController {
 
