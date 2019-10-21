9e711cd4acd395b415090b5202eb1d336e9daad1
Sandra Valero Ponte <svalerop@vectoritcgroup.com>
2019-10-21 12:43:35 +0200

    21 octubre

--- src/main/java/com/project/controllers/AnimalController.java
+++ src/main/java/com/project/controllers/AnimalController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.ui.Model;
 
@@ -21,87 +23,122 @@ import com.project.repositories.IUsuarioRepository;
 
 import java.util.List;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Controller
 @RequestMapping("/animales")
 public class AnimalController {
 
 	@Autowired
 	private IAnimalRepository animalesRepo;
-	
+
 	@Autowired
 	private IProvinciaRepository provinciasRepo;
-	
+
 	@Autowired
 	private IUsuarioRepository usuariosRepo;
-	
+
 	@GetMapping("/list")
 	public String pageList(Model model) {
-	
-			List<Animal> listaAnimales = animalesRepo.findAll();
-			//Tipo[] opcionesTipo = Tipo.values();
-			
-			
-	    	model.addAttribute("animales", listaAnimales);
-	    	model.addAttribute("tipos", Tipo.class);
-		 
-	    	return "animales/list";
-	}
-	
-	@GetMapping("/altaAnimal")//
-	public String pageAdd(Model model) {
-		 
-		Animal animal = new Animal();
-		
-		//Sexo[] opcionesSexo = Sexo.values(); No usados
-		//Tipo[] opcionesTipo = Tipo.values();
-		
-	
-		Esterilizado[] opcionesEsterilizado = Esterilizado.values(); 
-	
-		List<Provincia> listaProvincias = provinciasRepo.findAll();
-		List<Usuario> listaUsuarios = usuariosRepo.findAll();
-		
-		//si fuera un select, se pondría de esta forma: 
-		//model.addAttribute("tipos", opcionesTipo);
-		
-	 	model.addAttribute("animal", animal);
-	 	//model.addAttribute("tipos", Tipo.class);
-	 	//model.addAttribute("sexos", Sexo.class);
-	 	model.addAttribute("provincias", listaProvincias); 
-	 	model.addAttribute("esterilizados", opcionesEsterilizado); //esterilizados se envía al foreach de la vista
-	 	
-	      return "animales/altaAnimal";
-	}
-	
-	
-	@GetMapping("/bajaAnimal")
-	public String redBaja(Model model) {
-		Animal animal = new Animal();
+
 		List<Animal> listaAnimales = animalesRepo.findAll();
-		
-		Estado[] opcionesEstado = Estado.values();
-		
-		
-		model.addAttribute("animal", animal);
-		model.addAttribute("estados", opcionesEstado);
+		// Tipo[] opcionesTipo = Tipo.values();
+
 		model.addAttribute("animales", listaAnimales);
+		model.addAttribute("tipos", Tipo.class);
+
+		return "animales/list";
+	}
+
+	@GetMapping("/altaAnimal") //
+	public String pagAlta(Model model) {
+
+		Animal animal = new Animal();
+		model.addAttribute("animal", animal);
+
+		Esterilizado[] opcionesEsterilizado = Esterilizado.values();
+		model.addAttribute("esterilizados", opcionesEsterilizado);
+
+		List<Provincia> listaProvincias = provinciasRepo.findAll();
+		model.addAttribute("provincias", listaProvincias);
+
+		List<Usuario> listaUsuarios = usuariosRepo.findAll();
+
+		// No usados porque al final se decidió sacar los valores manualmente
+
+		// Sexo[] opcionesSexo = Sexo.values();
+		// model.addAttribute("sexos", Sexo.class);
+
+		// Tipo[] opcionesTipo = Tipo.values();
+		// model.addAttribute("tipos", Tipo.class);
+
+		// si fuera un select, en vez de ENUM se pondría de esta forma:
+		// model.addAttribute("tipos", opcionesTipo);
+
+		return "animales/altaAnimal";
+	}
+
+	@GetMapping("/altaAnimal-submit") //Lo que ocurre cuando pulsas el botón de guardar
+	public RedirectView pageAddSubmit(Animal animal, @RequestParam("file") MultipartFile file, Model model)
+			throws IllegalStateException, IOException {
 		
+		// TRATAMIENTO DE SUBIDA DE IMAGEN
+
+		// Guardamos la extension de la imagen
+		String extensionImagen = file.getOriginalFilename().split("\\.")[1];
+
+		// Generamos un nombre random a la imagen y la guardamos junto a la extension.
+		String nombreImagen = UUID.randomUUID().toString() + "." + extensionImagen;
+
+		// Guardamos la imagen en una carpeta del proyecto para imagenes.
+		String path = "C:\\Users\\svalerop\\Documents\\workspace-sts-3.9.10.RELEASE\\tfg_svp\\src\\main\\resources\\static\\images\\"
+				+ nombreImagen;
+
+		// Generamos una variable de tipo archivo a partir de la ruta y nombre del nuevo
+		// archivo (path), si no existe la ruta se crea.
+		File dirPath = new File(path);
+		if (!dirPath.exists()) {
+			dirPath.mkdirs();
+		}
 		
+		//Transferimos la imagen subida en el formulario a la ruta previamente indicada para guardarla. 
+		file.transferTo(dirPath);
 		
+		//Debemos asignar la variable foto del animal al nombre de la imagen subida. 
+		animal.setFoto(nombreImagen);
 		
+		//Y finalmente guardamos el objeto animal en la BD
+		animalesRepo.save(animal);
+
+		return new RedirectView("altaAnimalSubmit");
+	}
+
+	@GetMapping("/bajaAnimal")
+	public String pagBaja(Model model) {
+		Animal animal = new Animal();
+
+		List<Animal> listaAnimales = animalesRepo.findAll();
+		model.addAttribute("animales", listaAnimales);
+		model.addAttribute("animal", animal);
+
+		Estado[] opcionesEstado = Estado.values();
+		model.addAttribute("estados", opcionesEstado);
+
 		return "animales/bajaAnimal";
 	}
-	
+
 	@GetMapping("/modAnimal")
-	public String redMod(Model mod) {
+	public String pagMod(Model model) {
 		return "animales/modAnimal";
 	}
-	
+
 	@PostMapping("/add-submit")
 	public RedirectView pageAddSubmit(Animal animal, Model model) {
-	 		
+
 		animalesRepo.save(animal);
-		
-	 	return new RedirectView("animales/add-submit");
+
+		return new RedirectView("animales/add-submit");
 	}
 }
