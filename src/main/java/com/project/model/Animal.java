b91e1214a3aa64b7ef7be74080ff3589da5f2986
Sandra Valero Ponte <svalerop@vectoritcgroup.com>
2019-10-24 18:09:55 +0200

    Listados casi listos

--- src/main/java/com/project/model/Animal.java
+++ src/main/java/com/project/model/Animal.java
@@ -1,14 +1,16 @@
 package com.project.model;
+
 import java.text.ParseException;
 
-
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.TimeZone;
+import java.time.Period;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -34,65 +36,63 @@ import javax.persistence.GenerationType;
 import javax.persistence.Enumerated;
 import javax.persistence.ForeignKey;
 
-
-
-
-
-@Table(name="ANIMAL")
+@Table(name = "ANIMAL")
 @Entity
 public class Animal {
-	
-	//ATRIBUTOS
-	
+
+	// ATRIBUTOS
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID")
 	private int id;
-	
+
 	@Enumerated(EnumType.STRING)
-	@Column (name = "TIPO", nullable = false)
+	@Column(name = "TIPO", nullable = false)
 	private Tipo tipo;
-	
+
 	@Column(name = "NOMBRE", length = 50, nullable = false)
 	private String nombre;
-	
+
 	@Enumerated(EnumType.STRING)
-	@Column(name="SEXO", nullable = false)
+	@Column(name = "SEXO", nullable = false)
 	private Sexo sexo;
-	
+
 	@Column(name = "RAZA", length = 100, nullable = false)
 	private String raza;
 
 	@Column(name = "FNAC")
 	private LocalDate fnac;
 
-    //CLAVE FORANEA A TABLA PROVINCIA, 1-N
-	@ManyToOne(cascade=CascadeType.MERGE) @JoinColumn(name="PROVINCIA_ID", nullable=false, foreignKey = @ForeignKey(name = "FK_animal_provincia"))
-    private Provincia provincia;
-	
+	// CLAVE FORANEA A TABLA PROVINCIA, 1-N
+	@ManyToOne(cascade = CascadeType.MERGE)
+	@JoinColumn(name = "PROVINCIA_ID", nullable = false, foreignKey = @ForeignKey(name = "FK_animal_provincia"))
+	private Provincia provincia;
+
 	@Column(name = "POBLACION", length = 50, nullable = false)
 	private String poblacion;
-	
+
 	@Enumerated(EnumType.STRING)
-	@Column (name = "ESTERILIZADO", nullable = false)
+	@Column(name = "ESTERILIZADO", nullable = false)
 	private Esterilizado esterilizado;
-	
+
 	@Column(name = "FOTO", length = 200, nullable = false)
 	private String foto;
-	
+
 	@Column(name = "DESCRIPCION", length = 255, nullable = false)
 	private String descripcion;
-	
+
 	@Enumerated(EnumType.STRING)
-	@Column(name="ESTADO", nullable = false)
+	@Column(name = "ESTADO", nullable = false)
 	private Estado estado;
-	
-	//CLAVE FORANEA A TABLA USUARIO  (EN CASO DE ANIMAL ADOPTADO)
-	@ManyToOne(cascade=CascadeType.MERGE) @JoinColumn(name="USUARIO_ID", nullable=true, foreignKey = @ForeignKey(name = "FK_animal_usuario"))
-    private Usuario owner;
-	
-	//CONSTRUCTOR
-	
+
+	// CLAVE FORANEA A TABLA USUARIO (EN CASO DE ANIMAL ADOPTADO)
+	@ManyToOne(cascade = CascadeType.MERGE)
+	@JoinColumn(name = "USUARIO_ID", nullable = true, foreignKey = @ForeignKey(name = "FK_animal_usuario"))
+	private Usuario owner;
+
+	// CONSTRUCTOR
+
 	public Animal() {
 		super();
 		this.id = 0;
@@ -109,13 +109,12 @@ public class Animal {
 		this.estado = Estado.EN_ADOPCION;
 		this.owner = null;
 	}
-	
-	
 
-	//CONSTRUCTOR
-	
-	public Animal(int id, Tipo tipo, String nombre, String raza, String foto, Esterilizado esterilizado, String descripcion,
-			Provincia provincia, String poblacion, Sexo sexo, LocalDate fnac, Estado estado, Usuario owner) {
+	// CONSTRUCTOR
+
+	public Animal(int id, Tipo tipo, String nombre, String raza, String foto, Esterilizado esterilizado,
+			String descripcion, Provincia provincia, String poblacion, Sexo sexo, LocalDate fnac, Estado estado,
+			Usuario owner) {
 		this.id = id;
 		this.tipo = tipo;
 		this.nombre = nombre;
@@ -131,178 +130,149 @@ public class Animal {
 		this.owner = owner;
 	}
 
-
-	//GETTERS & SETTERS
+	// GETTERS & SETTERS
 
 	public int getId() {
 		return id;
 	}
 
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
 
 	public Tipo getTipo() {
 		return tipo;
 	}
 
-
-
 	public void setTipo(Tipo tipo) {
 		this.tipo = tipo;
 	}
-
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-
 
 	public String getRaza() {
 		return raza;
 	}
 
-
-
 	public void setRaza(String raza) {
 		this.raza = raza;
 	}
-
-
 
 	public String getFoto() {
 		return foto;
 	}
 
-
-
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
-
-
 
 	public Esterilizado getEsterilizado() {
 		return esterilizado;
 	}
 
-
-
 	public void setEsterilizado(Esterilizado esterilizado) {
 		this.esterilizado = esterilizado;
 	}
-
-
 
 	public String getDescripcion() {
 		return descripcion;
 	}
 
-
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
-
 
 	public Provincia getProvincia() {
 		return provincia;
 	}
 
-
-
 	public void setProvincia(Provincia provincia) {
 		this.provincia = provincia;
 	}
-
-
 
 	public String getPoblacion() {
 		return poblacion;
 	}
 
-
-
 	public void setPoblacion(String poblacion) {
 		this.poblacion = poblacion;
 	}
-	
 
-//		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	public String getFnacSpanish() {
+		String formattedDate = this.fnac.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+		return formattedDate;
+	}
+
+	public String getEdad() {
+		Period periodo = Period.between(fnac, LocalDate.now());
+		int anios = periodo.getYears();
+		int meses = periodo.getMonths();
+		int dias = periodo.getDays();
+		
+		if(anios>1) {
+			if(meses==0) {
+				return anios + " años";
+			}else if(meses==1){
+				return anios + " años y " + meses + " mes";
+			}else {
+				return anios + " años y " + meses + " meses";
+			}
+		}else if (anios==1) {
+			if(meses==0) {
+				return anios + " año";
+			}else if(meses==1){
+				return anios + " año y " + meses + " mes";
+			}else {
+				return anios + " año y " + meses + " meses";
+			}
+		}else if (dias==1) {
+			return dias + " día";
+		}else if (meses<1) {
+			return dias + " días";
+		}else if (meses==1) {
+			return meses + " mes";
+		}else if (meses<12) {
+			return meses + " meses";
+		}
+		return "afidfdh";
+	}
+
+	public void setFnac(String fnac) {
+		this.fnac = LocalDate.parse(fnac);
+	}
 
 	public String getFnac() {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		format.setTimeZone(TimeZone.getTimeZone("UTC+2"));
-		return format.format(this.fnac);
+		return fnac.toString();
 	}
-
-	public void setFnac(String fnac) throws ParseException {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		format.setTimeZone(TimeZone.getTimeZone("UTC+2"));
-		this.fnac = format.parse(fnac);
-	}
-	
-	public String fnacSpanish() {
-		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
-		format.setTimeZone(TimeZone.getTimeZone("UTC+2"));
-		return format.format(this.fnac);
-	}
-
-
-
-
-
 
 	public Sexo getSexo() {
 		return sexo;
 	}
 
-
-
 	public void setSexo(Sexo sexo) {
 		this.sexo = sexo;
 	}
-
-
 
 	public Estado getEstado() {
 		return estado;
 	}
 
-
-
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-
-
 
 	public Usuario getOwner() {
 		return owner;
 	}
 
-
-
 	public void setOwner(Usuario owner) {
 		this.owner = owner;
 	}
-	
-
-
-
-	
 
 }
